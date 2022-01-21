8b517a938fd42b9f57f205ddb70c41425358f6b8
Roger Vilà <rogervila@me.com>
2022-01-21 16:03:16 +0100

    local bulma and alpine  instead of cdn

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,12 @@
-
 import { createInertiaApp } from 'inertia-html'
+import Alpine from 'alpinejs'
 
 createInertiaApp({
     resolve: name => require(`./Pages/${name}.html`),
     setup({ el, isServer, load, props }) {
-        load({ target: el, isServer, props })
+        load({ target: el, isServer, props }).then(() => {
+            window.Alpine = Alpine
+            Alpine.start()
+        })
     },
 })
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,9 +5,9 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"> -->
     <script src="{{ mix('/js/app.js') }}" defer></script>
-    <script src="https://unpkg.com/alpinejs" defer></script>
+    <!-- <script src="https://unpkg.com/alpinejs" defer></script> -->
     <meta name="csrf_token" content="{{ csrf_token() }}">
     @inertiaHead
 </head>
